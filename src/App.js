d8c5a324560e1e909063b433c0713d1979c7c26d
soumen9775 <rakshitsoumen4@gmail.com>
2023-05-26 23:01:08 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from 'react'
 import { Resturant } from './component/Basic/Resturant';
+//import { UseState } from './component/Hooks/UseState';
+// import { UseEffect } from './component/Hooks/UseEffect';
+// import {UseReducer} from './component/Hooks/UseReducer';
 export const App = () => {
   return (
     <div>
       <Resturant />
+      {/* <UseState/> */}
+      {/* <UseEffect/> */}
+      {/* <UseReducer/> */}
     </div>
   )
 }
