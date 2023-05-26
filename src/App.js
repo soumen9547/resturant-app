02b708451b303fa4961c5c8a01eb5d9ec6de4e29
soumen9775 <rakshitsoumen4@gmail.com>
2023-05-26 20:42:39 +0530

    1st commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Resturant } from './component/Basic/Resturant';
+export const App = () => {
+  return (
+    <div>
+      <Resturant />
+    </div>
+  )
+}
+export default App;
